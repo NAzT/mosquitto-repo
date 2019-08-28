e369e61d4ac9b17924c83f6b164044bcd6700afb
Nat Weerawan <nat.wrw@gmail.com>
2019-08-28 16:07:42 +0700

    update

--- run.sh
+++ run.sh
@@ -2,25 +2,25 @@
 
 DIR=/home/nat/mosquitto-repo
 
-CONTAINER_NAME=mosquitto
+CONTAINER_NAME=mosquitto 
 
 VOL_LOG=mosquitto_log 
-VOL_DATA=mosquitto_data
-VOL_PASSWD=mosquitto_passwd
+VOL_USER=mosquitto_userspace
 
 docker volume create $VOL_LOG
-docker volume create $VOL_DATA
-
-docker volume create $VOL_PASSWD 
+docker volume create $VOL_USER
 # docker inspect mosquitto -f {{.HostConfig.Binds}}
 docker run -d -it --restart=always -p 1883:1883 -p 9001:9001 --name $CONTAINER_NAME \
-	-v $DIR/mosquitto.conf:/mosquitto/config/mosquitto.conf\
-	-v $VOL_DATA:/mosquitto/data\
-	-v $VOL_PASSWD:/mosquitto/passwords\
-       	-v $VOL_LOG:/mosquitto/log\
+	-v $DIR/mosquitto.conf:/mosquitto/config/mosquitto.conf \
+	-v $VOL_USER:/mosquitto/userspace \
+       	-v $VOL_LOG:/mosquitto/log \
        	eclipse-mosquitto
 
-docker cp "$DIR/user-space/passwd" mosquitto:/mosquitto/passwords/
+docker cp "$DIR/user-space/passwd" mosquitto:/mosquitto/userspace/
+docker cp "$DIR/user-space/bridges.d" mosquitto:/mosquitto/userspace/
 docker inspect $CONTAINER_NAME -f {{.HostConfig.Binds}}
-#sudo sh -c "truncate -s 0 /var/lib/docker/containers/*/*-json.log"
+
+docker restart mosquitto
+docker logs -f mosquitto
+#sudo sh -c "truncate -s 0 /var/lib/docker/containers/*/*-json.log" 
 #sudo sh -c "ls -ahl /var/lib/docker/containers/*/*-json.log"
